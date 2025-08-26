e3c8465d5f55d74b14ce66d2c2f1b9abc32a9373
Amitoshdeep <amitosh1999@gmail.com>
2025-08-26 14:01:36 +0530

    common css

--- src/components/G2Pic.jsx
+++ src/components/G2Pic.jsx
@@ -3,12 +3,13 @@ import React from 'react'
 function G2Pic() {
   return (
     // QUick Links GRID 2
-    <div className="quickCards rounded-4xl grid grid-cols-4 grid-rows-3 gap-2 w-full h-full p-4 items-center justify-center">
+    <div className="quickCards grid-cols-4 grid-rows-3">
         
-        <div className='flex flex-col h-full w-full items-center justify-center'>
-          <i class="fi fi-rr-plus bg-white h-full w-full flex items-center justify-center rounded-2xl"></i>
+        <div className='qCards'>
+          <i class="fi fi-rr-plus"></i>
           ADD
         </div>
+
 
     </div>
   )
